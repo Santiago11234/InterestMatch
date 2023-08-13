6c66c8b559002b52cff1db0707589a5195dcaa2f
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2023-08-13 14:19:13 -0500

    forgot to add put to server.js

--- finder-backend/server.js
+++ finder-backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
     origin: ['http://localhost:3000', 'https://interest-match-front.vercel.app'],
-    methods: ['GET', 'POST', 'DELETE'],
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true
   }));
   
--- foot-finder/src/Services/axios.js
+++ foot-finder/src/Services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://interest-match.vercel.app'
+    baseURL: 'https://interest-match.vercel.app/'
 })
 
 export default instance;
